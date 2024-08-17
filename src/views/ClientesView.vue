bdb8866ef7a80cbf0f16674ab364cc5f80c7143f
lionel-prats <lionel.prats.c@gmail.com>
2024-08-17 07:59:35 -0300

    192. Instalando FormKit y nuestro primer Formulario

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,28 @@
+<script setup>
+    // import {RouterLink} from "vue-router"
+    import RouterLink from "../components/UI/RouterLink.vue"
+    import Heading from "../components/UI/Heading.vue"
+
+    defineProps({
+        titulo: {
+            type: String,
+        }
+    })
+
+</script>
+<template>
+    <div>
+        <div class="flex justify-end">
+            <!-- <RouterLink 
+                :to="{name: 'agregar-cliente'}"
+                class="block md:inline-block rounded bg-blue-600 py-2 px-3"    
+            >    
+                Agregar CLiente
+            </RouterLink> -->
+            <RouterLink to="agregar-cliente">
+                Agregar CLiente
+            </RouterLink>
+        </div>
+        <Heading>{{titulo}}</Heading>
+    </div>
+</template>
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import {} from "@formkit/vue"
+
+    import RouterLink from "../components/UI/RouterLink.vue"
+    import Heading from "../components/UI/Heading.vue"
+
+    defineProps({
+        titulo: {
+            type: String,
+        }
+    })
+
+</script>
+<template>
+    <div class="flex justify-end">
+        <RouterLink to="inicio">
+            Volver
+        </RouterLink>
+    </div>
+    <Heading>{{titulo}}</Heading>
+    <div class="mx-auto mt-10 bg-white shadow">
+        <FormKit
+            type="form"
+        >
+        <!-- :actions="false" -->
+
+        </FormKit>
+    </div>
+</template>
